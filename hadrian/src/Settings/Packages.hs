b91e1fe2575fe99986d592bb30072f1a4a1a3406
Cheng Shao <terrorjack@type.dance>
2024-06-04 11:44:20 +0000

    hadrian: remove OSDarwin mention from speedHack
    This commit removes mentioning of OSDarwin from speedHack, since
    speedHack is purely for i386 and we no longer support i386 darwin
    (#24921).

--- hadrian/src/Settings/Packages.hs
+++ hadrian/src/Settings/Packages.hs
@@ -482,7 +482,7 @@ rtsPackageArgs = package rts ? do
 speedHack :: Action Bool
 speedHack = do
     i386   <- anyTargetArch [ArchX86]
-    goodOS <- not <$> anyTargetOs [OSDarwin, OSSolaris2]
+    goodOS <- not <$> anyTargetOs [OSSolaris2]
     return $ i386 && goodOS
 
 -- See @rts/ghc.mk@.
